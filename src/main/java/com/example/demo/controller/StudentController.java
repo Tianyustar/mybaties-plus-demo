caa229eee1aeb741345af015a90f9353fb3859d3
tianyuStar <yunxing.liu@foxmail.com>
2018-04-14 21:33:45 +0800

    添加注册接口，并且测试成功

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,9 +1,16 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.entity.Student;
+import com.example.demo.service.IStudentService;
+import com.example.demo.utils.StringUtil;
+import com.example.demo.utils.TMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.rmi.StubNotFoundException;
 
 /**
  * <p>
@@ -13,9 +20,27 @@ import org.springframework.stereotype.Controller;
  * @author libre
  * @since 2018-04-14
  */
-@Controller
+@RestController
 @RequestMapping("/student")
 public class StudentController {
 
+    private final IStudentService iStudentService;
+    @Autowired
+    public StudentController(IStudentService iStudentService) {
+        this.iStudentService = iStudentService;
+    }
+    @RequestMapping("register")
+    public TMessage  register(Student student)
+    {
+        if (StringUtil.isEmpty(student.getStudentName() )|| StringUtil.isEmpty(student.getPassword())) {
+            return new TMessage(TMessage.CODE_FAILURE, "用户名或密码不能为空");
+        }
+        return iStudentService.register(student);
+    }
+
+    @RequestMapping("test")
+    public String test() {
+        return   "test";
+    }
 }
 
